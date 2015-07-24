6c9a483680cfbcd0c463f6a6759a883ab6e62fcb
Giuseppe Acito <giuseppe.acito@gmail.com>
2015-07-24 12:10:56 +0200

    rimuovo i require da .Rprofile: non fanno girare il ciclo di build (manca setNames? what the hell

---
+++ tests/testthat/test.persistenza.r
@@ -0,0 +1,19 @@
+context("Funzioni per la persistenza")
+
+g <- GrafoDB("test")
+g["A"] <- TSERIES(runif(10), START=c(1990,1), FREQ=4)
+g["B"] <- TSERIES(runif(10), START=c(1990,1), FREQ=4)
+g["C"] <- function(A,B) {
+  C = A + B    
+}
+
+
+test_that("Posso salvare e ricaricare da un file", {
+  path <- tempfile()
+  saveBinary(g, path)
+  x <- readBinary(path)
+  expect_true(is.grafodb(x))
+  unlink(path)
+})
+
+
