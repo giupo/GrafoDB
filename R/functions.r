76008093bcde85c690bdadf0cc9e36482d3256c8
Giuseppe Acito <giuseppe.acito@bancaditalia.it>
2016-03-23 14:39:51 +0100

    Non modifico il tag eliminando i progressivo, non serve piu'

--- R/functions.r
+++ R/functions.r
@@ -25,7 +25,7 @@
   .Object@touched <- character(0)
   .Object@ordinal <- if(grepl("p(\\d+)$", tag)) {
     mth <- str_match(tag, "p(\\d+)$")
-    tag <- gsub(paste0(mth[1,1], "$"), "", tag)
+    #tag <- gsub(paste0(mth[1,1], "$"), "", tag)
     as.numeric(mth[1,2])
   } else {
     0
