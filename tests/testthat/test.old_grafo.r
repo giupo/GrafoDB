b7af008160a54bfd15d7f750d63a88b67538a99e
Giuseppe Acito <giuseppe.acito@gmail.com>
2017-02-15 12:03:32 +0100

    Added more tests

---
+++ tests/testthat/test.old_grafo.r
@@ -0,0 +1,93 @@
+context("Old API")
+
+
+setup <- function(tag) {
+  g <- GrafoDB(tag)
+  g["A"] <- ts(runif(10), start=c(1990,1), frequency=4)
+  g["B"] <- ts(runif(10), start=c(1990,1), frequency=4)
+  g["C"] <- function(A, B) {
+    C = A + B
+  }
+  g["D"] <- function() {
+    D <- ts(runif(10), start=c(1990,1), frequency=4)
+  }
+
+  setMeta(g, "A", "KEYA", "VALUE")
+  setMeta(g, "B", "KEYA", "VALUE")
+  
+  g
+  
+}
+
+test_that("isPrimitive returns true if a series is primitive (only numbers, no function)", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+  expect_true(isPrimitive(g, "A"))
+  expect_true(isPrimitive(g, "B"))
+  expect_false(isPrimitive(g, "C"))
+  expect_false(isPrimitive(g, "D"))
+})
+
+test_that("isElementary returns true if a series is elementary (numbers and formula, no parents)", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+  expect_false(isElementary(g, "A"))
+  expect_false(isElementary(g, "B"))
+  expect_false(isElementary(g, "C"))
+  expect_true(isElementary(g, "D"))
+})
+
+
+test_that("isAggregate returns true if a series is elementary (numbers and formula and parents)", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+  expect_false(isAggregate(g, "A"))
+  expect_false(isAggregate(g, "B"))
+  expect_true(isAggregate(g, "C"))
+  expect_false(isAggregate(g, "D"))
+})
+
+test_that("listNodes returns all the names of the Graph", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+  expect_equal(listNodes(g), names(g))
+  expect_true(all(listNodes(g) %in% c("A", "B", "C", "D")))
+})
+
+test_that("getDependencies return parents of an aggregate", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+  expect_equal(getDependencies(g, "C"), deps(g, "C"))
+  expect_true(all(getDependencies(g, "C") %in% c("A", "B")))
+})
+
+test_that("getTask returns the formula of the aggregate", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+
+  expect_equal(getTask(g, "C"), expr(g, "C"))
+  expect_equal(getTask(g, "C"), "C = A + B")
+  expect_equal(getTask(g, "A"), expr(g, "A"))  
+})
+
+test_that("searchNode returns a list of nodes matching search criteria", {
+  on.exit({
+    elimina("test")
+  })
+  g <- setup("test")
+
+  expect_equal(searchNode(g, "KEYA", "VALUE"), lookup(g, "KEYA", "VALUE"))
+  expect_error(searchNode(g, "NONESISTO", "NONESISTO"))
+})
