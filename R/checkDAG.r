2bc5d32475090eece76388400f9e8c52ba671d91
Giuseppe Acito <giuseppe.acito@gmail.com>
2020-02-06 10:45:02 +0100

    removing some imports

--- R/checkDAG.r
+++ R/checkDAG.r
@@ -1,9 +1,9 @@
 #' @importFrom igraph V E is.dag vertex topological.sort edge
 
 checkDAG <- function(network) {
-  if (!is.dag(network)) {
-    wrongsort <- try(topological.sort(network), silent=TRUE)
-    network_seq <- V(network)
+  if (!igraph::is.dag(network)) {
+    wrongsort <- try(igraph::topological.sort(network), silent=TRUE)
+    network_seq <- igraph::V(network)
     cycles_seq <- network_seq[setdiff(network_seq, network_seq[wrongsort])]
     cycles_vertex <- cycles_seq$name
     stop("Cycles found: ", paste(unlist(cycles_vertex), collapse=", "))
