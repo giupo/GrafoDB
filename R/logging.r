16438cf99c1cb2e7f1d0e458c9215f214f4fd0f3
Giuseppe Acito <giuseppe.acito@gmail.com>
2020-02-07 13:14:26 +0100

    removed importFrom crayon

--- R/logging.r
+++ R/logging.r
@@ -1,7 +1,3 @@
-
-
-#' @importFrom crayon red green blue yellow silver 
-
 layout.colored <- function(level, msg, ...) {
   the.time <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
   if (length(list(...)) > 0) {
@@ -13,17 +9,17 @@ layout.colored <- function(level, msg, ...) {
   nlevel <- names(level)
 
   proxy.level <- if(nlevel == "FATAL") {
-    red
+    crayon::red
   } else if (nlevel == "ERROR") {
-    red
+    crayon::red
   } else if (nlevel == "WARN") {
-    yellow
+    crayon::yellow
   } else if (nlevel == "INFO") {
-    white
+    crayon::white
   } else if (nlevel == "DEBUG") {
-    green
+    crayon::green
   } else {
-    silver
+    crayon::silver
   }
   
   sprintf("%s [%s] %s\n", proxy.level(names(level)), the.time, proxy.message(msg))
