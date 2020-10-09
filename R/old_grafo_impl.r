7ecd13d924de64345b767f84976b4d04ffb265f7
giupo <giuseppe.acito@gmail.com>
2020-10-09 18:07:18 +0200

    execute perm on autotest.r

--- R/old_grafo_impl.r
+++ R/old_grafo_impl.r
@@ -248,9 +248,6 @@ setMethod(
     nameObject <- deparse(substitute(object))
     .saveGraph(object, path, ...)
     object <- GrafoDB(path)
-    if(object@tag == path) {
-      assign(nameObject, object, envir=parent.frame())
-    }
     invisible(object)
   })
 
