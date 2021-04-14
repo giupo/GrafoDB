da750abcc2e2661aeeef855e7c7e1d0ffd11bdff
giupo <giuseppe.acito@gmail.com>
2021-04-14 15:49:20 +0200

    linted update_functions.r

--- R/update_functions.r
+++ R/update_functions.r
@@ -1,6 +1,6 @@
 #' @include logging.r
 
-update_functions <- function(x, con, tag=x@tag, msg="") {
+update_functions <- function(x, con, tag = x@tag, msg = "") {
   ln <- "GrafoDB::update_functions"
   if (interactive()) info("Update Functions ...", name = ln)
 
@@ -14,49 +14,50 @@ update_functions <- function(x, con, tag=x@tag, msg="") {
     DBI::dbGetQuery(con, sql_by_key(
       helper, "GET_CHANGED_FORMULE",
       tag = tag,
-      last_updated=as.numeric(timestamp)))
+      last_updated = as.numeric(timestamp)))
   } else {
     data.frame()
   }
 
-  names.with.conflicts <- intersect(x@touched, as.character(df$name))
+  names_with_conflicts <- intersect(x@touched, as.character(df$name))
 
-  names.updated <- setdiff(hash::keys(x@functions), names.with.conflicts)
-  if (length(names.updated)) {
-    formule <- foreach::`%dopar%`(foreach::foreach(name = iterators::iter(names.updated), .combine=rbind), {
-      formula <- expr(x, name, echo=FALSE)
+  names_updated <- setdiff(hash::keys(x@functions), names_with_conflicts)
+  if (length(names_updated)) {
+    formule <- foreach::`%dopar%`(foreach::foreach(
+      name = iterators::iter(names_updated), .combine = rbind), {
+      formula <- expr(x, name, echo = FALSE)
       cbind(formula, autore, name, tag)
     })
 
     if (DBI::dbExistsTable(con, paste0("formule_", tag)) ||
          class(con) == "SQLiteConnection") {
       foreach::`%do%`(foreach::foreach(row = iterators::iter(formule, "row")), {
-        formularow <- row[,1]
-        namerow <- row[,3]
+        formularow <- row[, 1]
+        namerow <- row[, 3]
 
         DBI::dbExecute(con, sql_by_key(
           helper, "UPDATE_FORMULE",
           tag = tag,
-          autore=autore,
-          formula=formularow,
-          name=namerow,
+          autore = autore,
+          formula = formularow,
+          name = namerow,
           msg = msg,
-          last_updated=time_in_nano()))
+          last_updated = time_in_nano()))
       })
     }
 
-    foreach::`%do%`(foreach::foreach(name = iterators::iter(names.updated)), {
-      formula <- expr(x, name, echo=FALSE)
+    foreach::`%do%`(foreach::foreach(name = iterators::iter(names_updated)), {
+      formula <- expr(x, name, echo = FALSE)
       DBI::dbExecute(con, sql_by_key(
         helper, "UPSERT_FORMULE",
-        formula=formula,
-        autore=autore,
-        name=name,
+        formula = formula,
+        autore = autore,
+        name = name,
         tag = tag,
         msg = msg,
-        last_updated=time_in_nano()))
+        last_updated = time_in_nano()))
     })
   }
-  
+
   if (interactive()) info("Update Functions done.", name = ln)
 }
