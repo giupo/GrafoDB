c84d7d065adbacbe1c3a982afdcc39e556f53dad
giupo <giuseppe.acito@gmail.com>
2021-04-14 16:50:34 +0200

    linted test.sqlhelper.r

--- tests/testthat/test.sqlhelper.r
+++ tests/testthat/test.sqlhelper.r
@@ -1,20 +1,20 @@
 context("SQLHelper functions")
 
 test_that("I can init an sql helper", {
-  sqlHelper <- SQLHelper()
-  expect_true(class(sqlHelper) == "SQLHelper")
+  sql_helper <- SQLHelper()
+  expect_true(class(sql_helper) == "SQLHelper")
 })
 
 test_that("SQLHelper defaults to PostgreSQL without env", {
 
   called <<- FALSE
-  with_mock('getenv' = function(...) {
+  with_mock("getenv" = function(...) {
     called <<- TRUE
     "prod"
   }, {
-    sqlHelper <- SQLHelper()
+    sql_helper <- SQLHelper()
     expect_true(called)
-    expect_equal(sqlHelper@type, "PostgreSQL")
+    expect_equal(sql_helper@type, "PostgreSQL")
   })
 })
 
@@ -22,7 +22,7 @@ test_that("SQLHelper defaults to PostgreSQL without env", {
 test_that("SQLHelper sets based on env", {
   called <<- FALSE
   with_mock(
-    'getenv' = function(...) {
+    "getenv" = function(...) {
       called <<- TRUE
       "test"
     }, {
@@ -38,47 +38,50 @@ test_that("SQLHelper raises an error if sql key is not present", {
 })
 
 test_that("SQLHelper gets different queries based on type", {
-  oldOption <- getOption("SQLHelperType", NULL)
-  sqlite <- SQLHelper(type="SQLite")
-  psql <- SQLHelper(type="PostgreSQL")
+  sqlite <- SQLHelper(type = "SQLite")
+  psql <- SQLHelper(type = "PostgreSQL")
   expect_true(sqlite@type != psql@type)
-  
+
   sql0 <- sql_by_key(sqlite, "SELECT_TEST0")
   sql1 <- sql_by_key(psql, "SELECT_TEST0")
   expect_true(sql0 != sql1)
-  
+
   sql0 <- sql_by_key(sqlite, "SELECT_TEST1")
   sql1 <- sql_by_key(psql, "SELECT_TEST1")
   expect_equal(sql0, sql1)
 })
 
 test_that("I can have params to be substitued into sql", {
-  helper <- SQLHelper(type="SQLite")
+  helper <- SQLHelper(type = "SQLite")
   expect_equal(helper@type, "SQLite")
 
   expect_equal(
-    sql_by_key(helper, "SELECT_TEST2", tabella="A"),
+    sql_by_key(helper, "SELECT_TEST2", tabella = "A"),
     "select * from A")
 
   expect_equal(
-    sql_by_key(helper, "SELECT_TEST2", tabella="B"),
+    sql_by_key(helper, "SELECT_TEST2", tabella = "B"),
     "select * from B")
 })
 
 test_that("SQLHelper can handle quoted values", {
-  helper <- SQLHelper(type="SQLite")
-  sql <- sql_by_key(helper, "SELECT_TEST3", tabella = "A", name= "d'aglio")
+  helper <- SQLHelper(type = "SQLite")
+  sql <- sql_by_key(helper, "SELECT_TEST3", tabella = "A", name = "d'aglio")
   expect_equal(sql, "select * from A where name='d''aglio'")
 })
 
 test_that("SQLHelper can handle multiple quoted values", {
-  helper <- SQLHelper(type="SQLite")
-  sql <- sql_by_key(helper, "SELECT_TEST3", tabella = "A", name = "d'odio e d'amore")
+  helper <- SQLHelper(type = "SQLite")
+  sql <- sql_by_key(helper,
+    "SELECT_TEST3",
+    tabella = "A",
+    name = "d'odio e d'amore")
+
   expect_equal(sql, "select * from A where name='d''odio e d''amore'")
 })
 
 test_that("SQLHelper raises an error if params are not set", {
-  helper <- SQLHelper(type="SQLite")
+  helper <- SQLHelper(type = "SQLite")
   expect_warning(expect_error(
     sql_by_key(helper, "SELECT_TEST3"),
     "params for query SELECT_TEST3 of type SQLite have not been set"))
