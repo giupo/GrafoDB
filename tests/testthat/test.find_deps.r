cb450af52fff0df3e9f36fae5df3705498026cf9
giupo <giuseppe.acito@gmail.com>
2021-04-14 16:25:22 +0200

    linted find_deps.r

--- tests/testthat/test.find_deps.r
+++ tests/testthat/test.find_deps.r
@@ -2,76 +2,69 @@
 context("is_error")
 
 test_that("test that is_error works", {
-    tryCatch({
-        stop("hello!")
-    }, error=function(cond) {
-        expect_true(is_error(cond))
-    })
+  tryCatch({
+    stop("hello!")
+  }, error = function(cond) {
+    expect_true(is_error(cond))
+  })
 
-    expect_false(is_error()) 
-    expect_false(is_error(NA)) 
-    expect_false(is_error(1))
-    expect_false(is_error(list())) 
-    expect_false(is_error(data.frame())) 
-    expect_false(is_error(FALSE))
-    expect_false(is_error(""))
+  expect_false(is_error())
+  expect_false(is_error(NA))
+  expect_false(is_error(1))
+  expect_false(is_error(list()))
+  expect_false(is_error(data.frame()))
+  expect_false(is_error(FALSE))
+  expect_false(is_error(""))
 })
 
 context("find_dep_from_error")
 
 test_that("find_dep_from_error doesn't work with anything but errors", {
-    
-    tryCatch({
-        stop("pippo pluto paperino: object 'X' not found")
-    }, error=function(cond) {
-        expect_equal(find_dep_from_error(cond), "X")
-    })
-
-
-    tryCatch({
-        stop("pippo pluto paperino: object 'x' not found")
-    }, error=function(cond) {
-        expect_equal(find_dep_from_error(cond), "x")
-        expect_false(find_dep_from_error(cond) ==  "X")
-    })
-
-    tryCatch({
-        stop("pippo pluto paperino")
-    }, error=function(cond) {
-        expect_true(is.na(find_dep_from_error(cond)))    
-    })
-    
-    expect_error(find_dep_from_error(), "`cond` is not an error")
-    expect_error(find_dep_from_error(NA), "`cond` is not an error")
-    expect_error(find_dep_from_error(1),"`cond` is not an error")
-    expect_error(find_dep_from_error(list()), "`cond` is not an error")
-    expect_error(find_dep_from_error(data.frame()), "`cond` is not an error")
-    expect_error(find_dep_from_error(FALSE), "`cond` is not an error")
-    expect_error(find_dep_from_error(""), "`cond` is not an error")    
+  tryCatch({
+    stop("pippo pluto paperino: object 'X' not found")
+  }, error = function(cond) {
+    expect_equal(find_dep_from_error(cond), "X")
+  })
+  tryCatch({
+    stop("pippo pluto paperino: object 'x' not found")
+  }, error = function(cond) {
+    expect_equal(find_dep_from_error(cond), "x")
+    expect_false(find_dep_from_error(cond) ==  "X")
+  })
+  tryCatch({
+    stop("pippo pluto paperino")
+  }, error = function(cond) {
+    expect_true(is.na(find_dep_from_error(cond)))
+  })
+  expect_error(find_dep_from_error(), "`cond` is not an error")
+  expect_error(find_dep_from_error(NA), "`cond` is not an error")
+  expect_error(find_dep_from_error(1), "`cond` is not an error")
+  expect_error(find_dep_from_error(list()), "`cond` is not an error")
+  expect_error(find_dep_from_error(data.frame()), "`cond` is not an error")
+  expect_error(find_dep_from_error(FALSE), "`cond` is not an error")
+  expect_error(find_dep_from_error(""), "`cond` is not an error")
 })
-
 
 context("find_deps")
 
 test_that("find_deps throws an error if objects are not found", {
-    data <- list(x=1, y=2)
-    formula <- "dd = x + y + b"
-    expect_error(find_deps(data, formula), "I dunno where to look for: b")
+  data <- list(x = 1, y = 2)
+  formula <- "dd = x + y + b"
+  expect_error(find_deps(data, formula), "I dunno where to look for: b")
 })
 
 test_that("find_deps works as expected", {
-    data <- list(x=1, y=2)
+  data <- list(x = 1, y = 2)
+  formula <- "dd = x + y"
+  expect_equal(find_deps(data, formula), c("x", "y"))
+  expect_false("dd" %in% find_deps(data, formula))
 
-    formula <- "dd = x + y"
-    expect_equal(find_deps(data, formula), c("x", "y"))
-    expect_false("dd" %in% find_deps(data, formula))
-    
-    formula <- "dd = x"
-    expect_equal(find_deps(data, formula), "x")
-    expect_false("dd" %in% find_deps(data, formula))
+  formula <- "dd = x"
+  expect_equal(find_deps(data, formula), "x")
+  expect_false("dd" %in% find_deps(data, formula))
 })
 
 test_that("if formula is NULL, deps are NULL", {
-   data <- list(x=1, y=2)
-   expect_null(find_deps(data, NULL))
+  data <- list(x = 1, y = 2)
+  expect_null(find_deps(data, NULL))
 })
